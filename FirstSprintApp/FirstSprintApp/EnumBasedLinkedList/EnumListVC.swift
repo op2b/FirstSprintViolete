765154f317fb581f680db048e73e0017e23f86bb
got2b <rsupanda@gmail.com>
2019-10-27 22:15:48 +0300

    небольшая кастомизация для вторго VC

--- FirstSprintApp/FirstSprintApp/EnumBasedLinkedList/EnumListVC.swift
+++ FirstSprintApp/FirstSprintApp/EnumBasedLinkedList/EnumListVC.swift
@@ -45,8 +45,10 @@ class EnumListVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    listLabel.layer.cornerRadius = 30
+        let primaryColor = UIColor(red: 210/255, green: 109/255, blue: 128/255, alpha: 1)
+        let secondaryColor = UIColor(red: 107/255, green: 148/255, blue: 230/255, alpha: 1)
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        listLabel.layer.cornerRadius = 30
         
         
     }
